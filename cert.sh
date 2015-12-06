c702905697863fefacb5cdaf625e8ee1264b487a
Denny Dai <i@daihan.cn>
2015-12-06 14:16:22 +0800

    beautify cert.sh

--- cert.sh
+++ cert.sh
@@ -3,16 +3,12 @@ path=$(pwd)
 echo -n "Your Domain:"
 read domain
 docker-compose up -d
-cd $path/certs/
 openssl genrsa 4096 > $path/certs/$domain.key
-openssl req -new -sha256 -key $domain.key -subj "/CN=$domain" > $path/certs/$domain.csr
-cd $path/
-python $path/acme_tiny.py --account-key account.key --csr $path/certs/$domain.csr --acme-dir $path/www/ > $path/certs/$domain.crt
+openssl req -new -sha256 -key $path/certs/$domain.key -subj "/CN=$domain" > $path/certs/$domain.csr
+python $path/acme_tiny.py --account-key account.key --csr $path/certs/$domain.csr --acme-dir $path/www/ > $path/certs/pre.$domain.crt
 wget -O - https://letsencrypt.org/certs/lets-encrypt-x1-cross-signed.pem > $path/certs/letsencrypt-intermediate.pem
-cat $path/certs/$domain.crt $path/certs/letsencrypt-intermediate.pem > $path/certs/$domain.pem
+cat $path/certs/pre.$domain.crt $path/certs/letsencrypt-intermediate.pem > $path/certs/$domain.crt
 rm $path/certs/letsencrypt-intermediate.pem
-rm $path/certs/$domain.crt
-cat $path/certs/$domain.pem > $path/certs/$domain.crt
-rm $path/certs/$domain.pem
+rm $path/certs/pre.$domain.crt
 rm $path/certs/$domain.csr
 docker-compose stop
